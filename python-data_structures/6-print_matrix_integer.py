6f8a6692b3109d51071ebfe1c407bba716de084d
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-02 12:33:15 +0000

    Task 6: List of lists = Matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for line in matrix:
+        for number in line:
+            print("{:d}".format(number), end="")
+            if number != line[-1]:
+                print(" ", end="")
+        print()

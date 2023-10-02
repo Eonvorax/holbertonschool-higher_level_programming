d3c468bdd009d17df7c6ea4c1617dea6b075fa42
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-02 09:19:39 +0000

    Task 3: print integer list in reverse

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    # Decrementing by 1 from (len - 1) to -1 (not included)
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))

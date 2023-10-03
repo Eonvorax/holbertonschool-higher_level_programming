2167b67c1ed1c971bfd5437e98b01fefb920c4ee
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-03 08:15:37 +0000

    Task 5: Number of keys

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a given dictionary.
+
+    Args:
+    - a_dictionary: the dictionary to be evaluated
+
+    Returns:
+    - the number of keys in the dictionary
+    """
+    return len(a_dictionary)

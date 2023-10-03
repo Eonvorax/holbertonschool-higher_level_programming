506902d6d791acc7d68bd784166cdddd10a826ec
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-03 13:47:28 +0000

    Edits: comments, redundant code

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,26 +9,23 @@ def roman_to_int(roman_string):
 
     Returns:
         int: Decimal notation of the Roman numeral,
-        None if the input is invalid.
+        or None if the input is invalid.
     """
     if (not roman_string or not isinstance(roman_string, str)):
         # input is either None, or not a string ; returning default value
         return 0
 
     # Simple translation table from roman numerals to decimals
-    table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    table = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
 
     decimal = 0
     length = len(roman_string)
 
     for i in range(0, length):
-        if (roman_string[i] not in ("IVXLCDM")):
-            # Input string is not a valid roman numeral
-            return 0
         # Ugly but had to respect 80-char limit
         if ((i + 1) < length
                 and table[roman_string[i]] < table[roman_string[i + 1]]):
-            # Special case: subtract 1, then keep going to add next numeral
+            # Special case: subtract numeral, then keep going to next numeral
             decimal -= table[roman_string[i]]
         else:
             # Usual case: find numeral value in the table and add to the sum

e90f18c6f83ec70f3a220b436abad0836e5d4d55
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-04 13:25:00 +0000

    Task 102: Delete by value

---
+++ python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    """
+    Deletes keys with a specific value in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to delete keys from.
+        value: The value to search for in the dictionary.
+
+    Returns:
+        The modified dictionary with the specified keys removed.
+    """
+    if not a_dictionary:
+        return None
+
+    # Creating a list of the keys to delete
+    keys_to_delete = []
+    # Going through the dictionary
+    for key, val in a_dictionary.items():
+        if val == value:
+            # Found key to delete later, adding it to the list
+            keys_to_delete.append(key)
+
+    # Deleting all keys on the deletion list
+    for key in keys_to_delete:
+        del a_dictionary[key]
+
+    return a_dictionary

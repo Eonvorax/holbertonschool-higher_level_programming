b7fbece327726b2f813f06386d85a8ba911f520b
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-09 09:21:47 +0000

    Task 1: Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        size (int): The size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size

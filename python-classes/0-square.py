a2042cdd4aea9f73b2970771f404a7b5b471b3d0
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-09 09:08:33 +0000

    Setup dir, README + Task 0: My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines the Square class
+"""
+
+
+class Square:
+    """
+    This is an empty class named Square, with no methods or attributes yet.
+    """
+    pass

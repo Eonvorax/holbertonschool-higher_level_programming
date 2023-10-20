ec8292d8a1df61d864bad407f7b9bba02d6491d9
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-20 17:10:22 +0000

    Edit: comments typos

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -20,19 +20,19 @@ def pascal_triangle(n):
 
     # Creating n rows of Pascal's triangle, from the top down
     for i in range(0, n):
-        # creating the current row, empty for now
+        # Creating the current row, empty for now
         row = []
 
         for j in range(0, i + 1):
-            # Depending on the index (first and last element or not ?)
+            # Depending on the index (first/last element or not ?)
             if j in (0, i):
                 # First or last element of the row: defaults to 1
                 row.append(1)
             else:
                 # Using previous row to fill the middle of the current one
                 row.append(rows[i - 1][j] + rows[i - 1][j - 1])
-                # NOTE: This only happens after i > 2. Previous rows [1],[1,1]
-                # were built, so no index error is possible
+                # NOTE: This only happens after i >= 2. Previous rows
+                # [1],[1,1] were built, so no index error is possible
 
         # Adding completed row to the list
         rows.append(row)

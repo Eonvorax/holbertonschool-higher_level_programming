9391d47f3acec5f73278a86ba1df448417dcbee7
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-10-23 09:24:45 +0000

    Task 1: First Rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""
+This is the rectangle module
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """
+    This class represents a rectangle.
+
+    Attributes:
+        __width (int): The width of the rectangle.
+        __height (int): The height of the rectangle.
+        __x (int): The x-coordinate of the rectangle's position.
+        __y (int): The y-coordinate of the rectangle's position.
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor method for the Rectangle class
+        """
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """
+        Getter method for the width attribute.
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for the width attribute.
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter method for the height attribute.
+        """
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for the height attribute.
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter method for the x attribute.
+        """
+        return (self.__x)
+
+    @x.setter
+    def x(self, value):
+        """
+        Setter method for the x attribute.
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter method for the y attribute.
+        """
+        return (self.__y)
+
+    @y.setter
+    def y(self, value):
+        """
+        Setter method for the y attribute.
+        """
+        self.__y = value

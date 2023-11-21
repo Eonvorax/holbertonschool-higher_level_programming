d39b9332f398f4d2792a514bd909f55ffedd9cca
Eonvorax <mail4bishop@gmail.com>
2023-11-21 06:25:27 -0800

    Task 11: Genre id, LEFT JOIN

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- List all shows contained in hbtn_0d_tvshows that have
+-- at least one genre linked, or display NULL if they don't have one
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
